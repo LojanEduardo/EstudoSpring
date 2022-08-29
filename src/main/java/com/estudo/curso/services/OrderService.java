c7debdde7d6b35ac763ca431825c09d3efcde6a3
Lojan Eduardo <lojaneduardo@gmail.com>
2022-08-29 11:50:42 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/estudo/curso/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.estudo.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudo.curso.entities.Order;
+import com.estudo.curso.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository OrderRepository;
+	
+	public List<Order> findAll(){
+		return OrderRepository.findAll();
+	}
+	
+	public Order findById(Long id){
+		Optional<Order> obj = OrderRepository.findById(id);
+		
+		return obj.get();
+	}
+	
+}
